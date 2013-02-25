91d75807f9faef8c6ae18276112919ed3cd31639
Rex <rex@bittorrent.com>
2013-02-25 00:56:38 -0800

    first release

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,32 +1,43 @@
-define(['jquery', 'underscore', 'backbone'], function() {
+define(['underscore', 'backbone'], function() {
 	var TipperView = Backbone.View.extend({
-	    	DISPLAY_MAX_LENGTH: 7,
 	    	el: $('#tipper'),
 	        events: {
-	        	'click .dailpad .btn[data-role="fn"]': 'fnButtonClicked',
-	        	'click .dailpad .btn[data-role="num"]': 'numButtonClicked',
-	        	'click .dailpad .btn[data-role="pt"]': 'ptButtonClicked',
-	        	'click .dailpad .btn[data-role="clear"]': 'clearButtonClicked',
-                'click .dailpad .btn[data-role="split"]': 'splitButtonClicked'
+	        	'touchstart .dailpad .btn[data-role="fn"]': 'fnButtonClicked',
+	        	'touchstart .dailpad .btn[data-role="num"]': 'numButtonClicked',
+	        	'touchstart .dailpad .btn[data-role="pt"]': 'ptButtonClicked',
+	        	'touchstart .dailpad .btn[data-role="clear"]': 'clearButtonClicked',
+                'touchstart .dailpad .btn[data-role="split"]': 'splitButtonClicked'
 	        },
 
 	        initialize: function() {
-	        	this.on('changeinput', this.onInputChange);
-	        	this.on('changeoutput', this.onOutputChange);
+	        	console.log('initilizing tipper');
+	        	this.hideAddressBar();
+	        	this.on('updateinput', this.onInputUpate);
+	        	this.on('updateoutput', this.onOutputUpdate);
+	        	this.on('reset', this.onReset);
+	        	$('#tipper').on('touchmove', this.stopScrolling);
+	        	$('#tipper').on('touchstart', this.stopScrolling);
+	        },
+
+	        hideAddressBar: function() {
+				if (document.height <= window.outerHeight + 10) {
+				document.body.style.height = (window.outerHeight + 60) + 'px';
+				setTimeout(function() {window.scrollTo(0, 1);}, 50);
+				} else {
+				setTimeout(function() {window.scrollTo(0, 1);}, 0);
+				}
+    		},
+
+	        stopScrolling: function(e) {
+	        	e.preventDefault();
 	        },
 
 	        ptButtonClicked: function(e) {
-	        	var currentInput = $('.display[data-role="input"] p').text();
-
-	        	if (currentInput.indexOf('.') !== -1) {
-	        		return;
-	        	}
-
-	        	this.updateInput(currentInput + '.');
+	        	this.trigger('updateinput', '.');
 	        },
 
 	        clearButtonClicked: function(e) {
-	        	this.updateInput('0');
+	        	this.trigger('reset');
 	        },
 
 	        fnButtonClicked: function(e) {
@@ -35,39 +46,15 @@ define(['jquery', 'underscore', 'backbone'], function() {
 
 	        	$('.dailpad div.btn-red').removeClass('btn-red');
 	        	me.addClass('btn-red');
-	        	this.trigger('changeoutput');
 
+	        	this.trigger('updateoutput');
 	        },
 
 	        numButtonClicked: function(e) {
 	        	var me = $(e.target),
-	        		num = me.attr('data-num'),
-	        		currentInput = $('.display[data-role="input"] p').text(),
-	        		pointIndex = currentInput.indexOf('.');
-	        	
-	        	if (pointIndex != -1 && (pointIndex > 5 || currentInput.length - pointIndex > 2)) {
-	        		return;
-	        	} else {
-	        		if (pointIndex === -1 && currentInput.length > 4) {
-	        			return;
-	        		}
+	        		num = me.attr('data-num');
 
-	        		if (currentInput === '0') {
-	        			if (num === '0') {
-	        				return;
-	        			} else {
-	        				currentInput = num;
-	        			}
-	        		} else {
-	        			currentInput = currentInput + num;
-	        		}
-	        	}
-
-	        	this.updateInput(currentInput);
-
-	        	function isInt(n) {
-	        		return n.valueOf() % 1 == 0 ? true: false;
-	        	}
+	        	this.trigger('updateinput', num);
 	        },
         
             splitButtonClicked: function(e) {
@@ -77,14 +64,15 @@ define(['jquery', 'underscore', 'backbone'], function() {
                 
                 $('.dailpad div.btn-blue').removeClass('btn-blue');
                 me.addClass('btn-blue');
-                this.trigger('changeoutput');
+
+                this.trigger('updateoutput');
             },
 
 	        updateInput: function(value) {
 	        	if (!isNumber(value)) return;
 
 	        	$('.display[data-role="input"] p').text(value);
-	        	this.trigger('changeinput', value);
+	        	this.trigger('updateoutput', value);
 
 	        	function isNumber(n) {
 	        		return !isNaN(parseFloat(n)) && isFinite(n);
@@ -100,27 +88,62 @@ define(['jquery', 'underscore', 'backbone'], function() {
 	        	}
 	        },
 
-	        onInputChange: function(value) {
-	        	value = parseFloat(value);
+	        onInputUpate: function(value) {
+	        	var	currentInput = $('.display[data-role="input"] p').text(),
+	        		pointIndex = currentInput.indexOf('.');
 
-	        	var percentage = parseInt($('.dailpad .btn-red[data-role="fn"]').attr('data-percentage'), 10) / 100,
-	        	// console.log(value, percentage, parseInt(percentage) / 100);
-	        		valueNew = (value + value * percentage).toFixed(2);
+	        	if (value === '.') {
+		        	if (pointIndex !== -1) {
+		        		return;
+		        	}
+		        	currentInput = currentInput + value;
+	        	} else {
+	        		if (!isInt(value)) {
+	        			return;
+	        		}
 
-	        	if (valueNew == '0.00') valueNew = '0';
+					if (pointIndex != -1 && (pointIndex > 5 || currentInput.length - pointIndex > 2)) {
+						return;
+					} else {
+						if (pointIndex === -1 && currentInput.length > 4) {
+							return;
+						}
 
-	        	this.updateOutput(valueNew);
+						if (currentInput === '0') {
+							if (value === '0') {
+								return;
+							} else {
+								currentInput = value;
+							}
+						} else {
+							currentInput = currentInput + value;
+						}
+					}
+	        	}
+	        	
+	        	this.updateInput(currentInput);
+
+	        	function isInt(n) {
+	        		return n.valueOf() % 1 == 0 ? true: false;
+	        	}
+
 	        },
 
-	        onOutputChange: function() {
+	        onOutputUpdate: function() {
 	        	var percentage = parseInt($('.dailpad .btn-red[data-role="fn"]').attr('data-percentage'), 10) / 100,
 	        	    splits = parseInt($('.dailpad .btn-blue[data-role="split"]').attr('data-splits'), 10),
                     currentInput = parseFloat($('.display[data-role="input"] p').text()),
 	        	   	valueNew = ((currentInput + currentInput * percentage) / splits).toFixed(2);
 
 	        	if (valueNew === '0.00') valueNew = '0';
+
 	        	this.updateOutput(valueNew);
+	        },
+
+	        onReset: function() {
+	        	this.updateInput('0');
 	        }
+
 	  	
 	    });  
 
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,9 +5,11 @@ require.config({
   paths: {
     jquery: 'vendor/jquery.min',
     backbone: '../components/backbone/backbone-min',
-    underscore: '../components/underscore/underscore-min'
+    underscore: '../components/underscore/underscore-min',
+  	add2home: 'vendor/add2home'
   }
 });
+require(['add2home']);
 require(['app'], function(module) {
 	var app = new module;
 });
